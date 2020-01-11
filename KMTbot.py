c323639fa5a304d009a0ae979a0d80ff3ad84449
Harold Lee <31803859+zttofficial@users.noreply.github.com>
2020-01-11 22:58:09 +0800

    Add files via upload

--- KMTbot.py
+++ KMTbot.py
@@ -6,12 +6,15 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 def officer(update, context):
-    context.bot.sendSticker(chat_id=update.effective_chat.id,
+    context.bot.send_message(chat_id=update.effective_chat.id,
                              sticker="CAADBQADIwAD4RTDG2se4dhpZE0DFgQ")
 
 def main():
     officer_handler = CommandHandler('officer', officer)
     dispatcher.add_handler(officer_handler)
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
     main()
