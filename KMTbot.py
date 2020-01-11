dd2963ead0ef970b18c4065b557c3a5c8271b405
Harold Lee <31803859+zttofficial@users.noreply.github.com>
2020-01-11 22:36:44 +0800

    Add files via upload

--- KMTbot.py
+++ KMTbot.py
@@ -1,43 +1,12 @@
-const TelegramBot = require('node-telegram-bot-api');
-const request = require('request');
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 
-const token = 'Your Token';
-const bot = new TelegramBot(token, {
-  polling: true
-});
+updater = Updater(token='TOKEN', use_context=True)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
-bot.onText(/\/officer/, function KMTofficer(msg) {
-  bot.sendSticker(msg.chat.id, 'CAADBQADIwAD4RTDG2se4dhpZE0DFgQ');
-});
+def officer(update, context):
+    context.bot.sendSticker(chat_id=update.effective_chat.id,
+                             sticker="CAADBQADIwAD4RTDG2se4dhpZE0DFgQ")
 
 
-bot.sendSticker(id, sticker[0]).then(message => {
-            let cid = message.chat.id;
-            let mid = message.message_id;
-            if (data.autodelete[cid] && data.lastid[cid]) {
-                bot.deleteMessage(cid, data.lastid[cid]);
-            }
-            data.lastid[cid] = mid;
-            saveData();
-        }))
-
- 
-if (e.parameter.runAutoTask){
-   AutoTask();
-   return HtmlService.createHtmlOutput(e.parameter.runAutoTask);
-}
-
-function AutoSendSticker() {
-  var payload = {
-    "method": "sendPhoto",
-    "chat_id": 539065210,
-    "sticker": "CAADBQADIwAD4RTDG2se4dhpZE0DFgQ"
-  };
- 
-  postTelegram(payload);
-}
- 
- 
-function AutoTask() {
-  AutoSendSticker();
-}
