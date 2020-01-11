54a5b91ade84c32639f865a55d2b1bf288dc25e0
Harold Lee <31803859+zttofficial@users.noreply.github.com>
2020-01-11 22:45:29 +0800

    Add files via upload

--- KMTbot.py
+++ KMTbot.py
@@ -9,4 +9,9 @@ def officer(update, context):
     context.bot.sendSticker(chat_id=update.effective_chat.id,
                              sticker="CAADBQADIwAD4RTDG2se4dhpZE0DFgQ")
 
+def main():
+    officer_handler = CommandHandler('officer', officer)
+    dispatcher.add_handler(officer_handler)
 
+if __name__ == '__main__':
+    main()
