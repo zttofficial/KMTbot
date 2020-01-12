7827a20250a85edc2f29abd1e8c722684f979a4d
Harold Lee <31803859+zttofficial@users.noreply.github.com>
2020-01-12 21:23:29 +0800

    Add files via upload

--- KMTbot.py
+++ KMTbot.py
@@ -2,16 +2,24 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 updater = Updater(token='TOKEN', use_context=True)
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+logger = logging.getLogger()
+
+def KMT(bot, update):
+    update.message.reply_text("Kuomintang")
 
 def officer(update, context):
-    context.bot.send_sticker(chat_id=update.effective_chat.id,
+    reply = bot.send_sticker(chat_id=update.effective_chat.id,
                              sticker="CAADBQADIwAD4RTDG2se4dhpZE0DFgQ")
+    update.message.reply_text(reply)
 
 def main():
     officer_handler = CommandHandler('officer', officer)
     dispatcher.add_handler(officer_handler)
+    updater.dispatcher.add_handler(CommandHandler('KMT', KMT))
+
 
     updater.start_polling()
     updater.idle()
